5c3a8a993e9405bee03a02bd79e377d396bafb40
Hajrudin Zelef <angeoulai2015@gmail.com>
2026-09-08 16:44:28 +0000

    fix(vision-agent): __init__.py imported non-existent ActionParser
    - parser.py exports function 'parse', not class 'ActionParser'
    - Fixed import to 'from .parser import parse as parse_action'

--- agent/__init__.py
+++ agent/__init__.py
@@ -1,5 +1,5 @@
 from .screenshot import ScreenCapture
 from .control import Control
 from .vision import Vision
-from .parser import ActionParser
+from .parser import parse as parse_action
 from .loop import AgentLoop
